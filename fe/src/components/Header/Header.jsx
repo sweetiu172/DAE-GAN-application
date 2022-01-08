bcf026509c1789ace75e3f64bec4dbab3495b62e
MinhTuan <tuan-lnm@genkisystem.com>
2022-01-08 13:37:57 +0700

    update landing page

--- fe/src/components/Header/Header.jsx
+++ fe/src/components/Header/Header.jsx
@@ -1,58 +1,143 @@
-import React, {useState} from 'react'
-import {
+// import React, {useState} from 'react'
+// import {
 
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem, Collapse
-} from 'reactstrap';
+//     Navbar,
+//     NavbarToggler,
+//     NavbarBrand,
+//     Nav,
+//     NavItem,
+//     NavLink,
+//     UncontrolledDropdown,
+//     DropdownToggle,
+//     DropdownMenu,
+//     DropdownItem, Collapse
+// } from 'reactstrap';
 
-// import { MenuItems } from "./MenuItems"
-// import { Button } from '../Button'
-// import './Navbar.css'
-const Header = () => {
-  const [isOpen, toggleIsOpen] = useState(false)
-  const toggle = () => {
-      toggleIsOpen(!isOpen)
-  }
+// // import { MenuItems } from "./MenuItems"
+// // import { Button } from '../Button'
+// // import './Navbar.css'
+// const Header = () => {
+//   const [isOpen, toggleIsOpen] = useState(false)
+//   const toggle = () => {
+//       toggleIsOpen(!isOpen)
+//   }
+//   return (
+//       <Navbar color="light" light expand="md">
+//         <NavbarBrand href="/">reactstrap</NavbarBrand>
+//         <NavbarToggler onClick = {toggle} />
+//         <Collapse isOpen={isOpen} navbar>
+//           <Nav className="mr-auto" navbar>
+//             <NavItem>
+//               <NavLink href="/tools">Components</NavLink>
+//             </NavItem>
+//             <NavItem>
+//               <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+//             </NavItem>
+//             <UncontrolledDropdown nav inNavbar>
+//               <DropdownToggle nav caret>
+//                 Options
+//               </DropdownToggle>
+//               <DropdownMenu end>
+//                 <DropdownItem>
+//                   Option 1
+//                 </DropdownItem>no
+//                 <DropdownItem>
+//                   Option 2
+//                 </DropdownItem>
+//                 <DropdownItem divider />
+//                 <DropdownItem>
+//                   Reset
+//                 </DropdownItem>
+//               </DropdownMenu>
+//             </UncontrolledDropdown>
+//           </Nav>
+//         </Collapse>
+//       </Navbar>
+//   )
+// }
+// export default Header
+
+
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
+import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Link as Scroll } from 'react-scroll';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    fontFamily: 'Nunito',
+  },
+  appbar: {
+    background: 'none',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+  icon: {
+    color: '#fff',
+    fontSize: '2rem',
+  },
+  colorText: {
+    color: '#5AFF3D',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: '4.5rem',
+  },
+  goDown: {
+    color: '#5AFF3D',
+    fontSize: '4rem',
+  },
+}));
+export default function Header() {
+  const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
   return (
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick = {toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/tools">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-  )
+    <div className={classes.root} id="header">
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>
+            My<span className={classes.colorText}>Island.</span>
+          </h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <br />
+            My<span className={classes.colorText}>Island.</span>
+          </h1>
+          <Scroll to="place-to-visit" smooth={true}>
+            <IconButton>
+              <ExpandMoreIcon className={classes.goDown} />
+            </IconButton>
+          </Scroll>
+        </div>
+      </Collapse>
+    </div>
+  );
 }
-export default Header
