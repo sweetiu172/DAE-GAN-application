e2f63a1333d5107d064dd2c884028329631ed051
MinhTuan <tuan-lnm@genkisystem.com>
2022-01-08 12:32:01 +0700

    tuan test

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 import {
-  BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
@@ -12,13 +11,11 @@ import Header from './components/Header/Header';
 function App() {
     return (
       <div className='App'>
-        <BrowserRouter>
-          <Header/>
-          <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/tools" element={<Tools />} />
-          </Routes>
-        </BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/tools" element={<Tools />} />
+        </Routes>
       </div>
     );
 }
