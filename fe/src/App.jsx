555f6f909734e0cfacac9e6d3425a9557533ea94
MinhTuan <tuan-lnm@genkisystem.com>
2022-01-08 11:39:02 +0700

    update router

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import Home from './components/Home/Home.jsx';
+import Tools from './components/Tools/Tools';
+import Header from './components/Header/Header';
 
 function App() {
     return (
       <div className='App'>
-        <Home/>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/tools" element={<Tools />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
 }
