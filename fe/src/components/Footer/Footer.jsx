548a6d039655f2f1cc4717c2eea1c8cd8e0388e0
MinhTuan <tuan-lnm@genkisystem.com>
2022-02-14 22:58:01 +0700

    final_commit

--- fe/src/components/Footer/Footer.jsx
+++ fe/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
       <div className="container">
         <div className="row">
           {/* Column1 */}
-          <div className="col">
+          <div className="col-4">
             <h4>Project repository</h4>
             <ui className="list-unstyled">
               <li>
@@ -21,14 +21,16 @@ function Footer() {
             </ui>
           </div>
           {/* Column2 */}
-          <div className="col">
+          <div className="col-8">
             <h4>Resource</h4>
             <ui className="list-unstyled">
-              <li>Tilte: DAE-GAN: Dynamic Aspect-aware GAN for Text-to-Image Synthesis</li>
-              <li>Author: Ruan, Shulan and Zhang, Yong and Zhang, Kun and Fan, Yanbo and Tang, Fan and Liu, Qi and Chen, Enhong</li>
-              <li>Booktitle: Proceedings of the IEEE/CVF International Conference on Computer Vision</li>
-              <li>Pages: 13960--13969</li>
-              <li>Year: 2021</li>
+              @inproceedings &#123;ruan2021dae,<br />
+                &emsp; title=&#123;DAE-GAN: Dynamic Aspect-aware GAN for Text-to-Image Synthesis&#125;,<br />
+                &emsp; author=&#123;Ruan, Shulan and Zhang, Yong and Zhang, Kun and Fan, Yanbo and Tang, Fan and Liu, Qi and Chen, Enhong&#125;,<br />
+                &emsp; booktitle=&#123;Proceedings of the IEEE/CVF International Conference on Computer Vision&#125;,<br />
+                &emsp; pages=&#123;13960--13969&#125;,<br />
+                &emsp; year=&#123;2021&#125;<br />
+                &#125;
               <li>Github repository:   
                 <a style={{color:'#fafafa'}} href="https://github.com/hiarsal/DAE-GAN" >
                    hiarsal/DAE-GAN
